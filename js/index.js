0fc9f7b7c0c946e4879304b438993ae31261254b
DutchDucksDevelopment <freekskanaal@gmail.com>
2025-10-14 10:06:32 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -250,7 +250,7 @@ function generatenotes() {
                 notescreenheader.className = "bolded notescreenheader";
                 notescreenheader.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" onclick="closenotescreen()"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>`
                 const notescreenheadertitle = document.createElement("p");
-                notescreenheadertitle.innerHTML = "Patch Notes " + note.title + " " + note.version;
+                notescreenheadertitle.innerHTML = "" + note.title + " " + note.version;
                 const notescreenhr = document.createElement("hr");
 
                 const notescreendetails = document.createElement("div");
