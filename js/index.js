9313bede0e261dfd9932e0a1f4c6750b660690fe
Dutch Ducks Development <freekskanaal@gmail.com>
2026-01-09 19:44:36 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -318,6 +318,19 @@ function closenotescreen() {
     while (notescreen.firstChild) {notescreen.removeChild(notescreen.firstChild)};
 }
 
+const iframebox = document.getElementById('iframebox');
+const img = document.getElementById('game-bg');
+const imgheight = img.offsetHeight
+const imgwidth = img.offsetWidth
+function skinsheader() {
+    resetHeaderSelected();
+    document.getElementById('faq').style.display = "flex";
+    document.getElementById('header3').classList.add('selected');
+    img.style.display = 'none';
+    iframebox.innerhtml = `<iframe src="./skineditor" width="${imageWidth}" height="${imageHeight}" frameborder="0"></iframe>`;
+    iframebox.style.display = 'block';
+    }
+
 // Game Edition Selected
 let launcher = "./assets/json/base.json";
 function webedition(){
